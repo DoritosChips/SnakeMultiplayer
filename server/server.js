29b27dd670d2626568c99726adbc4b32decd2a99
DoritosChips <98758239+DoritosChips@users.noreply.github.com>
2022-04-11 23:56:34 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,4 +1,8 @@
-const io = require('socket.io')();
+const io = require('socket.io')(cors: {
+        origin: true,
+        methods: ["GET", "POST"],
+        credentials: true
+      });
 const { initGame, gameLoop, getUpdatedVelocity } = require('./game');
 const { makeid } = require('./utils');
 const { FRAME_RATE } = require('./constants');
