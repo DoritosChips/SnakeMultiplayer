481c77ae4d9c7a7e27a5e677f30affbb43bd4659
DoritosChips <98758239+DoritosChips@users.noreply.github.com>
2022-04-12 14:51:28 +0300

    Add files via upload

--- frontend/index.js
+++ frontend/index.js
@@ -4,13 +4,17 @@ const APPLE_COLOR = 'red';
 
 const gameScreen = document.getElementById('gameScreen');
 const initialScreen = document.getElementById('initialScreen');
+const lobbyScreen = document.getElementById('lobbyScreen');
 const newGameButton = document.getElementById('newGameButton');
 const joinGameButton = document.getElementById('joinGameButton');
+const playButton = document.getElementById('playButton');
 const gameCodeInput = document.getElementById('gameCodeInput');
 const gameCodeDisplay = document.getElementById('gameCodeDisplay');
+const playersConnectedDisplay = document.getElementById('playersConnectedDisplay');
 
 newGameButton.addEventListener('click', newGame);
 joinGameButton.addEventListener('click', joinGame);
+playButton.addEventListener('click', play);
 
 function newGame() {
     socket.emit('newGame');
@@ -21,7 +25,12 @@ function joinGame() {
     socket.emit('joinGame', code);
 }
 
-const socket = io('https://limitless-brushlands-29107.herokuapp.com/');
+function play() {
+    const code = gameCodeDisplay.value;
+    socket.emit('play', code);
+}
+
+const socket = io('127.0.0.1:3000');
 
 socket.on('init', handleInit);
 socket.on('gameState', handleGameState);
@@ -29,6 +38,8 @@ socket.on('gameOver', handleGameOver);
 socket.on('gameCode', handleGameCode);
 socket.on('unknownGame', handleUnknownGame);
 socket.on('tooManyPlayers', handleTooManyPlayers);
+socket.on('playerJoined', handlePlayerJoined);
+socket.on('gameStarted', handleGameStarted);
 
 let canvas, ctx;
 let playerNumber;
@@ -36,7 +47,9 @@ let gameActive = false;
 
 function init() {
     initialScreen.style.display = 'none';
-    gameScreen.style.display = 'block';
+    lobbyScreen.style.display = 'block';
+
+    playersConnectedDisplay.innerText = '1';
 
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
@@ -130,4 +143,18 @@ function reset() {
     gameCodeDisplay.innerText = '';
     initialScreen.style.display = 'block';
     gameScreen.style.display = 'none';
+    lobbyScreen.style.display = 'none';
+    playButton.style.display = 'none';
+}
+
+function handlePlayerJoined(numPlayers) {
+    if (numPlayers > 1 && playerNumber === 1) {
+        playButton.style.display = 'block';
+    }
+    playersConnectedDisplay.innerText = numPlayers;
+}
+
+function handleGameStarted() {
+    lobbyScreen.style.display = 'none';
+    gameScreen.style.display = 'block';
 }
